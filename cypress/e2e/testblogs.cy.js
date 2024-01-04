bc2e2172faa85e078d5c42b495e41b8be315a267
frostyral <csjjgarcia@gmail.com>
2024-01-04 15:44:37 +0800

    developed loop for verifying image and title redirection with their respective blog titles

--- cypress/e2e/testblogs.cy.js
+++ cypress/e2e/testblogs.cy.js
@@ -18,19 +18,39 @@ describe('Testing Blogs Tab from Innovuze Solutions Inc.', () => {
         cy.get('section.blog div.row').should('be.visible').should('not.be.empty')
       })
 
-      it('Verify If Clicking the Shown Blogs is Working', () => {
+    it('Verify If Clicking the Shown Blogs is Working', () => {
         // Should Redirect to the blog page
         //cy.get('section.blog').should('be.visible').click()
-        cy.get('section.blog').find('img').its('length').should('eq', 5)
-        cy.get('section.blog').find('img').its('length').should('have.length', 3)
-        .each(($li, index, '$lis') => {
-            return 'something else'
-        })
-        .then(($lis) => {
-            expect($lis).to.have.length(3) // true
-        })
-      })
+        // check image redirect
+        cy.get('section.blog').find('img').each(($el, index, $listofElements) => {
+            cy.get('section.blog img').eq(index).click({force:true})
+            cy.go('back')
+          }
+        )
+        // check title redirects
+        cy.get('section.blog').find('h2.blog-title').each(($el, index, $listofElements) => {
+          cy.get('section.blog h2.blog-title a').eq(index).click()
+  
+            const currentText = $el.text();
+          
+            cy.get('h2.blog-page-title').eq(0).invoke('text').then((blogPageTitle) => {
+              const wordsArray = currentText.split(/\s+/);
+          
+              // Check if at least one word from currentText is present in the blog page title
+              const isAtLeastOneWordPresent = wordsArray.some((word) => blogPageTitle.includes(word));
+          
+              // Assert that at least one word from currentText is present in the blog page title
+              expect(isAtLeastOneWordPresent, `Expected "${blogPageTitle}" to include at least one word from "${currentText}"`).to.be.true;
+            });
+          
+            cy.go('back');
+          });
+      // check latest blog title redirect
+      cy.get('section.blog h2.latestBlog-title > a').click()
+    })
+    it('Verify if Read More Button is Working', () => {
+      // check latest blog read more redirects
+      cy.get('section.blog div.blogLatest-holder a.btn').click()
+    })
     
   })
-  
-  
